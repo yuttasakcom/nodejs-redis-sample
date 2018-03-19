4f5c27daa80cabb3f8437f22966870a74c924595
Yuttasak Pannawat <yuttasakcom@gmail.com>
2018-03-18 17:59:20 -0700

    create 04.pubsup

---
+++ examples/04.pubsub/index.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const socketIO = require('socket.io')
+const app = require('express')()
+const path = require('path')
+
+app.get('/', (_, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+const server = http.createServer(app)
+const io = socketIO(server)
+
+io.on('connection', socket => {
+  console.log('New user connect')
+
+  socket.on('chat', (msg, cb) => {
+    console.log(msg)
+    socket.emit('reply', msg)
+  })
+
+  socket.on('disconnect', () => console.log('User was disconnected'))
+})
+
+server.listen(3000, () => {
+  console.log('Server is running on port:3000')
+})
